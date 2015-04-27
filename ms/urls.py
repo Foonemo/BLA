6a5c3ad58ae4c9d979c473ffc2b2ded203c73c62
junhan <neil1028@gmail.com>
2015-04-27 11:29:34 -0500

    start learning css...

--- ms/urls.py
+++ ms/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-     # ex: /ms/5/art_piece
-    url(r'art_piece/', views.art_piece),#, name='art_piece'),
+     # ex: /ms/art_piece/
+    url(r'art_piece/', views.art_piece),
+     # ex: /ms/results/
+    url(r'results/', views.result),
 ]
--- ms/views.py
+++ ms/views.py
@@ -2,15 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArtPiece
 from django.template import RequestContext, loader, Context, Template
+from .forms.VeggieForms import SearchForm
 
 
-# Create your views here.
+def get_searchContent(request):
+    if request.method == 'GET':
+        form = SearchForm()
+    else:
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            return HttpResponseReidrect('/thanks/')
+
+    return render(request, 'ms/index.html', {'form': form,})
+
 def index(request):
-#    template = loader.get_template('ms/index.html')
+    return get_searchContent(request)
+
+
+def result(request):
     context = Context()
-    return render(request, 'ms/index.html', context)
+    return render(request, 'ms/results.html', context)
 
 
+
+# a simple test
 def art_piece(request):
     art_piece_list = ArtPiece.objects.raw('select * from art_piece order by art_id desc')
 #    template = loader.get_template('ms/art_piece.html')
