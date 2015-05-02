fc4e1a84604fbd305f8d89c0eb15b2568952cddc
junhan <neil1028@gmail.com>
2015-05-02 14:02:39 -0500

    redesigned url.py and templates htmls to hold different results

--- ms/urls.py
+++ ms/urls.py
@@ -4,11 +4,17 @@ from . import views
 urlpatterns = [
      # ex: /ms/
     url(r'^$', views.index, name='index'),
-     # ex: /ms/art_piece/
-#    url(r'^art_piece/$', views.art_piece),
      # ex: /ms/results/
     url(r'^results/$', views.results, name='results'),
+
+    # ex: /ms/results/events
+    url(r'^results/events/$', views.results_events, name='events'),
+    # ex: /ms/results/art_piece
+    url(r'^results/art_pieces/$', views.results_artpieces, name='art_piece'),
+
      # ex: /ms/thanks/
     url(r'^thanks/$', views.thanks, name='thanks'),
+
+
 
 ]
--- ms/views.py
+++ ms/views.py
@@ -7,6 +7,10 @@ from django.db import connection
 
 from .forms.VeggieForms import SearchForm, SelectRegionForm, SelectTypeForm, SelectStyleForm
 
+region_list = ["Asian", "African", "North American", "Central & South American", "Ocenanian", "European"]
+type_list = ["Applied & Decorative Arts","Drawing","Painting","Photograph","Print", "Sculpture","Watercolor"]
+style_list = ["Realist","Abstract","Expressionist","Conceptual"]
+
 def dictfetchall(cursor):
     "Returns all rows from a cursor as a dict"
     desc = cursor.description
@@ -17,9 +21,6 @@ def dictfetchall(cursor):
 
 
 def search_piece(query):
-
-       # res = ArtPiece.objects.raw("SELECT * FROM art_piece JOIN artist JOIN `create` ON art_piece.art_id = `create`.art_id AND `create`.artist_id=artist.artist_id WHERE art_name LIKE '%%' %s '%%' OR region LIKE '%%' %s '%%' OR style LIKE '%%' %s '%%' OR art_type LIKE '%%' %s '%%' OR description LIKE '%%' %s '%%' OR artist_name LIKE '%%' %s '%%' ", [query, query, query, query, query, query])
-
        cursor = connection.cursor()
        cursor.execute(
                ''' SELECT *
@@ -46,12 +47,6 @@ def search_piece(query):
 
        return res, row[0]
 
-# def search_res_num(query):
-#         cursor = connection.cursor()
-#         cursor.execute('''SELECT COUNT(*) FROM art_piece JOIN artist JOIN `create` ON art_piece.art_id = `create`.art_id AND `create`.artist_id=artist.artist_id WHERE art_name LIKE '%%' %s '%%' OR region LIKE '%%' %s '%%' OR style LIKE '%%' %s '%%' OR art_type LIKE '%%' %s '%%' OR description LIKE '%%' %s '%%' ''', [query, query, query, query, query])
-#         row = cursor.fetchone()
-#         return row[0]
-
 def search_event(query):
         res = Event.objects.raw("SELECT * FROM event WHERE event_name LIKE '%%' %s '%%'", [query])
 
@@ -66,28 +61,6 @@ def search_event(query):
         row = cursor.fetchone()
         return res, row[0]
 
-# def search_collections(region,style,s_type):
-# 	query=[]
-#         if not region == 'N/A':
-# 		query.append("region = " + region)
-
-#         if not style == 'N/A':
-# 		query.append("style = " + style)
-
-#         if not s_type == 'N/A':
-# 		query.append("s_type = " + s_type)
-
-#         if len(query) == 1:
-# 		return ArtPiece.objects.raw('select * from art_piece where %s',[query[0]])
-# 		#return return ArtPiece.objects.raw('select * from art_piece where %s',(query[0])
-# 	elif:
-# 		queries = query[0]
-#                 for q in query[1:]:
-#                     queries += " AND "
-#                     queries += q
-#                 return ArtPiece.objects.raw('select * from art_piece where %s',[queries])
-#         else:
-#             return ArtPiece.objects.raw('select * from art_piece')
 
 
 def index(request):
@@ -98,31 +71,27 @@ def index(request):
             search_query = form.cleaned_data['content']
 
             art_piece_list, piece_num= search_piece(search_query)
-            event_list, event_num = search_event(search_query)
+            # event_list, event_num = search_event(search_query)
 
             if len(list(art_piece_list)) <= 0:
                 art_piece_list = None
 
-            if len(list(event_list)) <= 0:
-                event_list = None
+            # if len(list(event_list)) <= 0:
+            #     event_list = None
 
             context = RequestContext(request, {
                     'art_piece_list': art_piece_list,
                     'piece_num': piece_num,
-                    'event_num': event_num,
-                    'event_list': event_list,
+                    # 'event_num': event_num,
+                    # 'event_list': event_list,
                     'query':search_query,
             })
 
-            return render(request, 'ms/results.html',context)
+            # return render(request, 'ms/results_artpieces.html',context)
+            return HttpResponseRedirect('./results/art_pieces/')
 
     else:
         form = SearchForm()
-
-    region_list = ["Asian", "African", "North American", "Central & South American", "Ocenanian", "European"]
-    type_list = ["Applied & Decorative Arts","Drawing","Painting","Photograph","Print", "Sculpture","Watercolor"]
-    style_list = ["Realist","Abstract","Expressionist","Conceptual"]
-
 
     return render(request, 'ms/index.html', {'form': form,
                                                'region_list': region_list,
@@ -131,19 +100,14 @@ def index(request):
 
 
 def results(request):
+    return render(request, 'ms/results.html', {})
 
-    return render(request, 'ms/results_d.html', {})
+def results_artpieces(request):
+    return render(request, 'ms/results_artpieces.html', {})
+
+def results_events(request):
+    return render(request, 'ms/results_events.html', {})
+
 
 def thanks(request):
     return render(request, 'ms/thanks.html', {})
-
-
-# a simple test
-# def art_piece(request):
-#     art_piece_list = ArtPiece.objects.raw('select * from art_piece order by art_id desc')
-# #    template = loader.get_template('ms/art_piece.html')
-#     context = RequestContext(request, {
-#         'art_piece_list': art_piece_list,
-#         })
-#     #    return HttpResponse(template.render(context))
-#     return render(request, 'ms/art_piece.html', context)
